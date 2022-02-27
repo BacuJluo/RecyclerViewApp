de937b46f9dac47d9c57073106f5f8e7a0a61bf3
BacuJluo <78647116+BacuJluo@users.noreply.github.com>
2022-02-27 12:56:56 +0300

    Создали recyclerView, адаптер для него, и его реализацию.

---
+++ app/src/main/java/com/home/recyclerviewapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.home.recyclerviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState ==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, SocialNetworkFragment.newInstance()).commit();
+        }
+    }
+    
+}
---
+++ app/src/main/java/com/home/recyclerviewapp/SocialNetworkFragment.java
@@ -0,0 +1,66 @@
+package com.home.recyclerviewapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class SocialNetworkFragment extends Fragment {
+
+
+    SocialNetworkAdapter socialNetworkAdapter;
+
+
+    public static SocialNetworkFragment newInstance() {
+        SocialNetworkFragment fragment = new SocialNetworkFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_social_network, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initAdapter();
+        initRecycler(view);
+
+
+        //Для того чтобы работал наш RecyclerView нужно сделать 3 вещи
+        //1.найти его в макете
+        //2.прописать его ЛаяутМэнеджер 41 строка.
+        //3.связать его со своим адаптером
+        // и в адаптере должны быть данные.
+    }
+
+    private void initAdapter() {
+        socialNetworkAdapter = new SocialNetworkAdapter();
+        socialNetworkAdapter.setData(getData());
+    }
+
+    void initRecycler(View view){
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager); //задали LinearLayoutManager
+        recyclerView.setHasFixedSize(true); //мы можем указать эту команду recyclerView что у него все элементы фиксированного размера и это ускорит работу inflater'a по надуванию нашего макета, и он не будет измерять каждый элемент, и это ускорит его работу.
+        recyclerView.setAdapter(socialNetworkAdapter);
+    }
+
+    String[] getData() {
+        String[] data = getResources().getStringArray(R.array.titles);
+        return data;
+    }
+}
